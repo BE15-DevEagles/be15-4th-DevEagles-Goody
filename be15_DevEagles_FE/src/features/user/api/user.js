4f36e753e32152c88b6ae64e6541786ee93509e2
nare20027 <nare20027@gmail.com>
2025-05-26 16:17:16 +0900

    fix : DEV-197-be-refresh-token-재발급
    - refreshToken 재발급 시 에러 발생 해결

--- be15_DevEagles_FE/src/features/user/api/user.js
+++ be15_DevEagles_FE/src/features/user/api/user.js
@@ -3,7 +3,6 @@ import { useAuthStore } from '@/store/auth.js';
 
 const exceptToken = [
   { method: 'post', url: '/auth/login' },
-  { method: 'post', url: '/auth/refresh' },
   { method: 'post', url: '/users$' },
   { method: 'post', url: '/users/duplcheck' },
   { method: 'patch', url: '/users/email/pwd' },
@@ -29,63 +28,6 @@ api.interceptors.request.use(
     return config;
   },
   error => {
-    return Promise.reject(error);
-  }
-);
-
-let isRefreshing = false;
-let failedQueue = [];
-
-const processQueue = (error, token = null) => {
-  failedQueue.forEach(p => {
-    if (error) p.reject(error);
-    else p.resolve(token);
-  });
-  failedQueue = [];
-};
-
-api.interceptors.response.use(
-  response => response,
-  async error => {
-    const originalRequest = error.config;
-    const authStore = useAuthStore();
-
-    if (error.response?.status === 401 && !originalRequest._retry) {
-      originalRequest._retry = true;
-
-      if (isRefreshing) {
-        return new Promise((resolve, reject) => {
-          failedQueue.push({ resolve, reject });
-        }).then(token => {
-          originalRequest.headers['Authorization'] = `Bearer ${token}`;
-          return api(originalRequest);
-        });
-      }
-
-      isRefreshing = true;
-
-      try {
-        const res = await api.post('/auth/refresh');
-        const newAccessToken = res.data.data.accessToken;
-
-        // accessToken 갱신
-        await authStore.setAuth(newAccessToken);
-        api.defaults.headers.common['Authorization'] = `Bearer ${newAccessToken}`;
-        originalRequest.headers['Authorization'] = `Bearer ${newAccessToken}`;
-
-        processQueue(null, newAccessToken);
-        return api(originalRequest);
-      } catch (refreshError) {
-        processQueue(refreshError, null);
-        authStore.clearAuth();
-        return Promise.reject(refreshError);
-      } finally {
-        isRefreshing = false;
-      }
-    }
-
-    console.log('[INTERCEPTOR] accessToken 갱신 성공');
-
     return Promise.reject(error);
   }
 );
