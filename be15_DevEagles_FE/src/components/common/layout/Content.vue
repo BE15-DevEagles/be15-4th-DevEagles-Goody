454948fee2e846bc2aa6d2a7a1e3f16e275da99f
Yangha Park <64etuor@gmail.com>
2025-05-26 11:11:31 +0900

    feat: DEV-199 팀 사이드바, 사이드바 선택, 호버 효과 적용, 채팅 목록 타팀 표시되던 문제 수정.

--- be15_DevEagles_FE/src/components/common/layout/Content.vue
+++ be15_DevEagles_FE/src/components/common/layout/Content.vue
@@ -4,14 +4,14 @@
       <div class="bg-[var(--color-neutral-white)] rounded-lg shadow-drop h-full p-4 overflow-auto">
         <div class="border-b border-[var(--color-gray-200)] pb-3 mb-4">
           <div class="flex items-center justify-between">
-            <h1 class="font-section-title">
-              <span class="text-[var(--color-gray-600)]">#</span>
-              {{ currentPage }}
-            </h1>
+            <div class="flex items-center space-x-3">
+              <h1 class="font-section-inner text-2xl">
+                <span class="text-[var(--color-gray-600)]">#</span>
+                {{ currentPage }}
+              </h1>
+              <span class="text-[var(--color-gray-500)] font-small">{{ pageDescription }}</span>
+            </div>
           </div>
-          <p class="text-[var(--color-gray-500)] font-one-liner mt-1">
-            {{ pageDescription }}
-          </p>
         </div>
 
         <div class="flex-grow overflow-y-auto space-y-4 mb-4">
